1dca7135cef637323ab7ec1e5b6504a35f72d8f6
Romain <rominoudu85@gmail.com>
2021-07-16 09:46:39 +0200

    Version 4

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,14 +10,22 @@ use Core\Form\Type\AFormType;
 use Core\Form\Type\SubmitType;
 use Core\Form\Type\TextAreaType;
 use Core\Form\Type\TextType;
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
 
 class ContactController extends AController
 {
 
-    public function __construct() {
+    private string $headTitle;
+
+    public function __construct()
+    {
+        $this->headTitle = 'Contact - Romain Royer';
     }
 
-    public function show() {
+    public function show()
+    {
         $formContact = new Form(
             [
                 'name' => new TextType(
@@ -65,11 +73,60 @@ class ContactController extends AController
             ]
         );
 
-        $headTitle = 'Contact - Romain Royer';
-
         $this->render('contact.html.twig', [
-            'headTitle' => $headTitle,
+            'headTitle' => $this->headTitle,
             'formContact' => $formContact
         ]);
     }
+
+    public function send()
+    {
+        // Create the Transport
+        $transport = (new Swift_SmtpTransport('smtp.ionos.fr', 587, 'tls'))
+            ->setUsername('ocp5@rominfo.fr')
+            ->setPassword('#G%r.+D5P9!zz;c')
+        ;
+
+        // Create the Mailer using your created Transport
+        $mailer = new Swift_Mailer($transport);
+
+        // Create a message
+        $messageAdmin = (new Swift_Message('Nouvelle demande de contact'))
+            ->setFrom(['contact@rominfo.fr' => 'Romain Royer'])
+            ->setTo(['contact@rominfo.fr', 'rominoudu85@gmail.com' => 'Romain Royer'])
+            ->setReplyTo([$_POST['email'] => $_POST['name']])
+            ->setBody(
+                '<p>Bonjour Romain,<br>Vous avez une nouvelle demande de contact.</p>
+                    <p>Nom : ' . $_POST['name'] . '<br>
+                    Adresse e-mail : ' . $_POST['email'] . '<br>
+                    Téléphone : ' . $_POST['phone'] . '<br>
+                    Message : <br>' . htmlentities($_POST['message']) . '</p>',
+                'text/html'
+            );
+
+        $messageUser = (new Swift_Message('Votre message a bien été envoyé.'))
+            ->setFrom(['contact@rominfo.fr' => 'Romain Royer'])
+            ->setTo([$_POST['email'] => $_POST['name']])
+            ->setBody(
+                '<p>Bonjour ' . $_POST['name'] . ',<br>Nous avons bien reçu votre message. Vous recevrez une réponse très rapidement.</p>
+                    <p>Voici votre message :<br>' . htmlentities($_POST['message']) . '</p>',
+                'text/html'
+            );
+
+        // Send the message
+        $result = $mailer->send($messageAdmin);
+        if ($result) {
+            $result = $mailer->send($messageUser);
+        }
+
+        if ($result) {
+            $this->render('contactConfirmation.html.twig', [
+                'headTitle' => $this->headTitle
+            ]);
+        } else {
+            $this->show();
+        }
+
+    }
+
 }
